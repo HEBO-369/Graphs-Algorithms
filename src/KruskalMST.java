7071554774e9354301d3bad9354a464c8c92a5c0
hebo-369 <m.whban369@gmail.com>
2026-05-19 18:54:07 +0300

    Added KruskalMST class for minimum spanning tree computation

---
+++ src/KruskalMST.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+public class KruskalMST
+{
+    private Queue<Edge> mst = new LinkedList<Edge>();
+    public KruskalMST(Graph G)
+    {
+        PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
+        for (Edge e : G.edges())
+            pq.add(e);
+        UF uf = new UF(G.V());
+        while (!pq.isEmpty() && mst.size() < G.V()-1)
+        {
+            Edge e = pq.poll();
+            int v = e.either(), w = e.other(v);
+            if (!uf.connected(v, w))
+            {
+                uf.union(v, w);
+                mst.add(e);
+            }
+        }
+    }
+    public Iterable<Edge> edges()
+    { return mst; }
+}
