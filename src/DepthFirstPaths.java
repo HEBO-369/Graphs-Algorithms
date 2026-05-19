3785337ac1ce7674a748972a5d4e9b3c21e4c74d
hebo-369 <m.whban369@gmail.com>
2026-05-19 18:46:33 +0300

    Added DepthFirstPaths class for pathfinding in a graph using DFS

---
+++ src/DepthFirstPaths.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Stack;
+
+public class DepthFirstPaths {
+    private boolean[] marked ;
+    private int[] edgeTo ;
+    private int s ;
+
+    public DepthFirstPaths(Graph G, int s) {
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        this.s = s;
+        dfs(G,s);
+    }
+    private void dfs(Graph G, int v) {
+        marked[v] = true;
+        for (Edge w : G.adj(v)) {
+            if(!marked[w.other(v)]){
+                edgeTo[w.other(v)] = v ;
+                dfs(G,w.other(v));
+            }
+        }
+    }
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+    public ArrayList<Integer> pathTo(int v) {
+        ArrayList<Integer> path = new ArrayList<Integer>();
+        if(!hasPathTo(v)){ return null ;}
+        else{
+            for (int x = v ; x!=s ; x =edgeTo[x]){
+                path.add(x);
+            }
+            path.add(s);
+            Collections.reverse(path);
+            return path;
+        }
+    }
+}
